3c04b07e50bbf724cc45f4f194035621fde86912
Rohit <rohitchaudharyrk.25@gmail.com>
2025-07-24 18:33:25 +0530

    commit

--- server/main.py
+++ server/main.py
@@ -75,7 +75,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://127.0.0.1:5173", "http://127.0.0.1:5174", "http://127.0.0.1:5175"],
+    allow_origins=["https://webforall-web-accessibility-for-everyone-production.up.railway.app","http://localhost:5173", "http://localhost:5174"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
